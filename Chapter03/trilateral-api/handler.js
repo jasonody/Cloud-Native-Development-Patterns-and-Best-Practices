a0777463435284003b4c4e6058368352904df193
Jason Ody <jasonody@Jasons-MacBook-Pro.local>
2018-12-22 19:06:51 -0800

    inserting an item to DynamoDB via a POST

--- Chapter03/trilateral-api/handler.js
+++ Chapter03/trilateral-api/handler.js
@@ -1,21 +1,8 @@
 'use strict';
 
-module.exports.command = (event, context, callback) => {
-  console.log('event: %j', event);
-
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: JSON.stringify({
-      message: 'Cloud Native Development Patterns and Best Practices! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-};
+const aws = require('aws-sdk')
+const uuid = require('uuid')
+const _ = require('highland')
 
 module.exports.query = (event, context, callback) => {
   console.log('event: %j', event);
@@ -39,7 +26,7 @@ module.exports.publish = (event, context, callback) => {
   callback();
 };
 
-module.exports.consume = (event, context, cb) => {
+module.exports.consume = (event, context, callback) => {
   console.log('event: %j', event);
   callback();
 };
