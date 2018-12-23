d0157a1f442785e2d11c8850f6858058ede1668a
Jason Ody <jasonody@Jasons-MacBook-Pro.local>
2018-12-22 19:22:38 -0800

    publishing item-inserted event

--- Chapter03/trilateral-api/handler.js
+++ Chapter03/trilateral-api/handler.js
@@ -23,8 +23,36 @@ module.exports.query = (event, context, callback) => {
 
 module.exports.publish = (event, context, callback) => {
   console.log('event: %j', event);
-  callback();
+
+  _(event.Records)
+    .flatMap(publishEvent)
+    .collect()
+    .toCallback(callback)
 };
+
+const publishEvent = (record) => {
+  const event = {
+    id: uuid.v1(),
+    type: 'item-created',
+    timestamp: Date.now(),
+    tags: {
+      userId: "User123"
+    },
+    record
+  }
+
+  const params = {
+    StreamName: process.env.STREAM_NAME,
+    PartitionKey: record.dynamodb.Keys.id.S,
+    Data: new Buffer(JSON.stringify(event)),
+  }
+
+  console.log('publish event params: %j', params)
+
+  const kinesis = new aws.Kinesis();
+
+  return _(kinesis.putRecord(params).promise())
+}
 
 module.exports.consume = (event, context, callback) => {
   console.log('event: %j', event);
